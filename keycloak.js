8ecf01ceba01fea6c4580a6d4729d3cc85186fd5
sragneau <sylvain.ragneau@nrco.fr>
2018-08-07 08:02:06 +0200

    Response HTTP error and stop flow return null

--- keycloak.js
+++ keycloak.js
@@ -1,19 +1,3 @@
-/**
- * Copyright 2018 Nicolas Carlier
- *
- * Licensed under the Apache License, Version 2.0 (the 'License')
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-
 module.exports = function (RED) {
   'use strict'
 
@@ -31,7 +15,7 @@ module.exports = function (RED) {
     this.client = n.client;
     this.verifier = {
       verify: function (accessToken, cb) {
-        return cb(RED._('auth-oidc.error.bad-discovery-endpoint'))
+        return cb(RED._('bad-discovery-endpoint'))
       }
     }
 
@@ -44,18 +28,18 @@ module.exports = function (RED) {
         this.status({
           fill: 'red',
           shape: 'ring',
-          text: 'auth-oidc.status.bad-discovery-request'
+          text: 'bad-discovery-request'
         })
-        return this.error(RED._('auth-oidc.error.bad-discovery-request'))
+        return this.error(RED._('bad-discovery-request'))
       }
       if (!body || !body.jwks_uri) {
         console.log('Discovery error: bad response: %j', body)
         this.status({
           fill: 'red',
           shape: 'ring',
-          text: 'auth-oidc.status.bad-discovery-response'
+          text: 'bad-discovery-response'
         })
-        return this.error(RED._('auth-oidc.error.bad-discovery-response'))
+        return this.error(RED._('bad-discovery-response'))
       }
       console.log('JWKS URI: %j', body.jwks_uri)
 
@@ -81,18 +65,19 @@ module.exports = function (RED) {
       this.status({
         fill: 'blue',
         shape: 'ring',
-        text: 'auth-oidc.status.ready'
+        text: 'ready'
       })
     })
 
     this.on('input', msg => {
       if (!msg.req || !msg.req.headers || !msg.req.headers['authorization']) {
-        this.error(RED._('auth-oidc.error.no-access-token'))
+        this.error(RED._('no-access-token'))
         this.status({
           fill: 'red',
           shape: 'ring',
-          text: 'auth-oidc.status.no-access-token'
-        })
+          text: 'no-access-token'
+        });
+        msg.payload = {};
         msg.error = 'NoAccessToken'
         msg.statusCode = 401
         return this.send(msg)
@@ -100,65 +85,74 @@ module.exports = function (RED) {
       const accessToken = msg.req.headers['authorization'].split(' ')[1]
       this.verifier.verify(accessToken, (err, jwt) => {
         if (err) {
-          this.error(RED._('auth-oidc.error.bad-access-token', err))
+          this.error(RED._('bad-access-token', err))
           this.status({
             fill: 'red',
             shape: 'ring',
-            text: 'auth-oidc.status.bad-access-token'
-          })
-          msg.error = err
-          msg.statusCode = 403
-          return this.send(msg)
+            text: 'bad-access-token'
+          });
+          delete msg.payload;
+          msg.error = err;
+          msg.statusCode = 403;
+          msg.res._res.status(msg.statusCode).send(msg.error);
+          return ;
         }
 
 
         msg.access_token = jwt;
         //check role and client
-        if (this.client && !jwt.body.resource_access[this.client]) {
+        console.log(jwt.body.resource_access[this.client]);
+        
+        if (this.client && !jwt.body.resource_access[this.client]) {         
           let err = 'Access error : client not found';
           this.status({
             fill: 'red',
             shape: 'ring',
             text: err
           });
-          msg.error = err
-          msg.statusCode = 403          
+          delete msg.payload;
+          msg.error = err;
+          msg.statusCode = 403;
+          msg.res._res.status(msg.statusCode).send(msg.error);
+          return ;
         }
-      
-        if (this.client && this.role && !jwt.body.resource_access[this.client].roles.has(this.roles)) {
+
+        
+        console.log()//.contains(this.role));
+        if (this.client && this.role && !jwt.body.resource_access[this.client].roles.includes(this.role)) {
           let err = 'Access error : role not found with client : ' + this.client;
           this.status({
             fill: 'red',
             shape: 'ring',
             text: err
           });
+          delete msg.payload;
           msg.error = err;
           msg.statusCode = 403;
-         
+          msg.res._res.status(msg.statusCode).send(msg.error);
+          return ;
+
         };
-      
+
         if (!this.client && this.role) {
           for (var client in jwt.body.ressource_access) {
-            if (client.roles.has(this.role)) {        
+            if (client.roles.includes(this.role)) {
               break;
             } else {
+              let err = 'Access error : role not found ';
+              this.status({
+                fill: 'red',
+                shape: 'ring',
+                text: err
+              });
+              delete msg.payload;
+              msg.error = err;
               msg.statusCode = 403;
+              msg.res._res.status(msg.statusCode).send(msg.error);
+              return ;
             }
-          }
-      
-          if (msg.statusCode == 403) {
-            let err = 'Access error : role not found ';
-            this.status({
-              fill: 'red',
-              shape: 'ring',
-              text: err
-            });
-            msg.error = err
-          }
-        };
-        if (delete msg.error) {
-          msg.payload = {};
-        }
+          }         
+        };      
 
         this.status({})
         this.send(msg)
@@ -167,4 +161,3 @@ module.exports = function (RED) {
   }
   RED.nodes.registerType('Keycloak', KeycloakNode)
 }
-
